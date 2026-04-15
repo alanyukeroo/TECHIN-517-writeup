cfd91f70ecd70cb6a08650f67ba6c0f24537bc49
alannur <alanyukeroo@gmail.com>
2026-04-14 20:51:34 -0700

    update command

--- docker/setup.sh
+++ docker/setup.sh
@@ -47,13 +47,6 @@ else
     ln -s "$PERSISTENT_HF_DIR" "$HF_CACHE_DIR"
 fi
 
-# install lerobot
-source /home/ubuntu/.bashrc
-cd /home/ubuntu/techin517/third_party/lerobot
-pip install 'lerobot[all]'
-pip uninstall torch torchvision torchaudio -y
-pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/cu128
-
 # build ros
 cd /home/ubuntu/techin517/ros2_ws/src/
 bash /home/ubuntu/techin517/ros2_ws/src/soa_ros2/build.sh
